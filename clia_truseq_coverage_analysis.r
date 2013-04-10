d3999693fc8fa3d792260cc443235d06e4238475
Mauricio Carneiro <carneiro@broadinstitute.org>
2013-04-10 09:24:47 -0400

    some updates to the clia script and general path changes in all scripts for uniformity

--- clia_truseq_coverage_analysis.r
+++ clia_truseq_coverage_analysis.r
@@ -3,39 +3,49 @@ require("ggplot2")
 uncovered <- function(data, t) {
   data_points = length(data$POS)
   percent_uncovered <- length(data[data$AVG_INTERVAL_DP/12 < t,]$POS) / data_points
-  cat(percent_uncovered, percent_uncovered * data_points)
+  percent_uncovered * data_points
 }
 
 load_downsampled_datasets = function (filename) {
-  master = cbind("10%", read.delim(paste("/humgen/gsa-hpprojects/dev/carneiro/clia/analysis/illumina_exome/downsample/down-0.1/", filename, sep = "")))
+  z <- data.frame()  
+  master <- cbind("100%", read.delim("/humgen/gsa-hpprojects/dev/carneiro/clia/analysis/illumina_exome/cds/master_cds_gencodeV15.intervals.tbl"))  
   names(master) <- c("dataset", names(master)[-1])
-  for (target in c(20, 30, 40, 50, 70, 80)) {
-    tmp <- cbind(paste(target, "%", sep=""), read.delim(paste("/humgen/gsa-hpprojects/dev/carneiro/clia/analysis/illumina_exome/downsample/down-0.", target/10, "/", filename, sep = "")))
+  for (target in c(90, 80, 70, 60, 50, 40, 30, 25, 20, 17, 15, 12, 10)) {
+    tmp <- cbind(paste(target, "%", sep=""), read.delim(paste("/humgen/gsa-hpprojects/dev/carneiro/clia/analysis/illumina_exome/downsample/down-0.", target, "/", filename, sep = "")))
     names(tmp) <- names(master)
-    master <- rbind(master, tmp)  
+    master <- rbind(tmp, master)  
+    z <- rbind(z, c((target/100) * median(original$AVG_INTERVAL_DP/12), uncovered(tmp, 20)))
   }
-  master
+  names(z) = c("ds", "uncovered")
+  list(master, z)
 }
 
 
 ## individual datasets
 
 original <- read.delim("/humgen/gsa-hpprojects/dev/carneiro/clia/analysis/illumina_exome/cds/master_cds_gencodeV15.intervals.tbl")
-#d <- read.delim("/Volumes/Lothlorien/Dropbox/sandbox/clia/untargeted.tbl")
-#d <- read.delim("/humgen/gsa-hpprojects/dev/carneiro/clia/analysis/illumina_exome/cancer/cancer.tbl")
+cser <- read.delim("/humgen/gsa-hpprojects/dev/carneiro/clia/analysis/illumina_exome/cser-genetest/GeneTest-transcripts-130221.intervals.vcf.tbl")
+canseq <- read.delim("/humgen/gsa-hpprojects/dev/carneiro/clia/analysis/illumina_exome/cancer/canseq_genes.tbl")
 
-qplot(AVG_INTERVAL_DP/12, data=d, geom="density", fill="red", xlab="Average depth per sample", ylab="% targets") + geom_vline(xintercept=20, linetype="dashed") + scale_fill_discrete(guide = "none")
+qplot(AVG_INTERVAL_DP/12, data=original, geom="density", fill="red", xlab="Average depth per sample", ylab="% targets") + geom_vline(xintercept=20, linetype="dashed") + scale_fill_discrete(guide = "none")
 qplot(FILTER, data=subset(d, FILTER != "PASS")) + theme(axis.text.x = element_text(angle=90, hjust=1))
 
 
 #Downsampled datasets
-d = load_downsampled_datasets("master_cds_gencodeV15.intervals.vcf.tbl")
+x = load_downsampled_datasets("master_cds_gencodeV15.intervals.vcf.tbl")
+d = x[[1]]
+z = x[[2]]
+rm(x)
+
 uncovered(d=subset(d, dataset=="10%"), 20)
 uncovered(d=subset(d, dataset=="20%"), 20)
 uncovered(d=subset(d, dataset=="30%"), 20)
 uncovered(d=subset(d, dataset=="40%"), 20)
 uncovered(d=subset(d, dataset=="50%"), 20)
+uncovered(d=subset(d, dataset=="60%"), 20)
 uncovered(d=subset(d, dataset=="70%"), 20)
 uncovered(d=subset(d, dataset=="80%"), 20)
+uncovered(d=subset(d, dataset=="90%"), 20)
 
-qplot(AVG_INTERVAL_DP/12, data=genetest, geom="density", fill=dataset, alpha=0.2, group=dataset, xlab="Average depth per sample", ylab="% targets") + geom_vline(xintercept=20, linetype="dashed") + scale_alpha(guide="none")
+qplot(AVG_INTERVAL_DP/12, data=subset(d, dataset=="10%" | dataset=="20%" | dataset=="30%" | dataset=="40%" | dataset=="50%"| dataset=="60%" | dataset=="70%" | dataset =="80%" | dataset == "90%" | dataset == "100%"), geom="density", fill=dataset, alpha=0.1, group=dataset, xlab="Average depth per target", ylab="% targets", xlim = c(0, 600)) + geom_vline(xintercept=20, linetype="dashed") + scale_alpha(guide="none") + scale_fill_hue(name="target coverage", labels=signif(seq(0.1, 1, 0.1) * median(original$AVG_INTERVAL_DP/12), digits=2))
+qplot(ds, uncovered, data=z, ylab="# targets under 20x coverage", xlab="target median coverage") + geom_smooth()
--- variants_per_sample_eomi.r
+++ variants_per_sample_eomi.r
@@ -1,5 +1,5 @@
 require("ggplot2")
-v <- read.table("/humgen/gsa-hpprojects/dev/carneiro/eomi/analysis/noReadPos/nVariantsBySample.tbl", quote="\"")
+# v <- read.table("/humgen/gsa-hpprojects/dev/carneiro/eomi/analysis/noReadPos/nVariantsBySample.tbl", quote="\"")
 v <- data.frame(cbind(c(1:length(v$V1)), v$V1))
 
 qplot(X1, X2,data=v, xlab="Sample index", ylab="Total Variants", main="Total number of variants per sample") + 
